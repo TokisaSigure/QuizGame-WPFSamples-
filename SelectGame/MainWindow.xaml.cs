4cbabf2fb54b4a7c9e2188dbc0109d50e25aa270
Takeuchiakira <s1223063@cce.kanagawa-it.ac.jp>
2014-12-22 00:14:26 +0900

    henkou1

--- SelectGame/MainWindow.xaml.cs
+++ SelectGame/MainWindow.xaml.cs
@@ -24,11 +24,20 @@ namespace SelectGame
         int ans;//解答を挿入するよう変数
         int num=0;//状態遷移用変数
         int quesnum = 0;//問題数格納用変数
+        DispatcherTimer dispatcherTimer;
         #endregion
 
         public MainWindow()
         {
             InitializeComponent();
+            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+
         }
 
 
